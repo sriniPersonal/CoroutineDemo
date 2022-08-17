cbdb508e21fc6f4a1ddf34d8195482d395e9cfbd
srinivas@quarterly.ai <srini.nitk@gmail.com>
2022-08-17 09:12:26 +0530

    updated java 11

--- app/src/main/java/com/example/coroutinedemo/MainActivity.kt
+++ app/src/main/java/com/example/coroutinedemo/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.coroutinedemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    val scope = CoroutineScope(Dispatchers.IO + CoroutineName("myScope"))
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //  each Coroutine has unique Job id
+        scope.launch {
+            Log.d("srini", this.coroutineContext.toString())
+            launch {
+                Log.d("srini", this.coroutineContext.toString())
+            }
+        }
+
     }
 }
